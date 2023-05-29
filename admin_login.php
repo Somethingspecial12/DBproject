d74dbdcc9918dec1e3645f029a189799488f95d2
Somethingspecial12 <somethingspecial1968@gmail.com>
2023-05-30 03:19:17 +0800

    first commit

---
+++ admin_login.php
@@ -0,0 +1,13 @@
+<?php 
+//it has to perform a checking operation. check and verify the username and the password parsed in the homepage and verify the admins
+
+//if (/*if the name and the password inputted by the admins matches the input in the database*/) {
+	//log them into the 2nd homepage/workpage. redirect to another site
+}
+//else(
+
+//throw an error message and remain in the same page.
+
+)
+
+ ?>
